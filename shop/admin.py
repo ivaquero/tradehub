8261f82e6da8134e468c90a71d3027216466279d
ivaquero <msaintms@outlook.com>
2025-09-12 18:43:29 +0800

    add urls

--- shop/admin.py
+++ shop/admin.py
@@ -7,20 +7,20 @@ from .models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display: ClassVar[list[str]] = ["name", "slug"]
-    prepopulated_fields: ClassVar[dict] = {"slug": ("name",)}
+    list_display: ClassVar[list[str]] = ["名称", "短标题"]
+    prepopulated_fields: ClassVar[dict] = {"短标题": ("名称",)}
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display: ClassVar[list[str]] = [
-        "name",
-        "slug",
-        "price",
-        "available",
-        "created",
-        "updated",
+        "名称",
+        "短标题",
+        "价格",
+        "有货",
+        "创建日期",
+        "更新日期",
     ]
-    list_filter: ClassVar[list[str]] = ["available", "created", "updated"]
-    list_editable: ClassVar[list[str]] = ["price", "available"]
-    prepopulated_fields: ClassVar[dict] = {"slug": ("name",)}
+    list_filter: ClassVar[list[str]] = ["有货", "创建日期", "更新日期"]
+    list_editable: ClassVar[list[str]] = ["价格", "有货"]
+    prepopulated_fields: ClassVar[dict] = {"短标题": ("名称",)}
