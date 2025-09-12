64df31b6e41947b9f2c5e4be2c67aad9d815b727
ivaquero <msaintms@outlook.com>
2025-09-12 17:12:16 +0800

    update models

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,26 @@
+from typing import ClassVar
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display: ClassVar[list[str]] = ["name", "slug"]
+    prepopulated_fields: ClassVar[dict] = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display: ClassVar[list[str]] = [
+        "name",
+        "slug",
+        "price",
+        "available",
+        "created",
+        "updated",
+    ]
+    list_filter: ClassVar[list[str]] = ["available", "created", "updated"]
+    list_editable: ClassVar[list[str]] = ["price", "available"]
+    prepopulated_fields: ClassVar[dict] = {"slug": ("name",)}
