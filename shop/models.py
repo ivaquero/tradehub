17615e3f3d0b10a2be6ed173b89db0cb93daee5f
ivaquero <msaintms@outlook.com>
2025-09-12 20:04:16 +0800

    update models

--- shop/models.py
+++ shop/models.py
@@ -8,10 +8,10 @@ class Category(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
-        ordering: ClassVar[list[str]] = ["名称"]
-        indexes: ClassVar[list] = [models.Index(fields=["名称"])]
-        verbose_name: ClassVar[str] = "类别"
-        verbose_name_plural: ClassVar[str] = "类别"
+        ordering: ClassVar[list[str]] = ["name"]
+        indexes: ClassVar[list] = [models.Index(fields=["name"])]
+        verbose_name: ClassVar[str] = "category"
+        verbose_name_plural: ClassVar[str] = "categories"
 
     def __str__(self):
         return self.name
@@ -19,11 +19,11 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(
-        Category, related_name="产品", on_delete=models.CASCADE
+        Category, related_name="products", on_delete=models.CASCADE
     )
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
-    image = models.ImageField(upload_to="产品/%Y/%m/%d", blank=True)
+    image = models.ImageField(upload_to="products/%Y/%m/%d", blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
@@ -31,11 +31,11 @@ class Product(models.Model):
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering: ClassVar[list[str]] = ["名称"]
+        ordering: ClassVar[list[str]] = ["name"]
         indexes: ClassVar[list] = [
-            models.Index(fields=["id", "短标题"]),
-            models.Index(fields=["名称"]),
-            models.Index(fields=["创建日期"]),
+            models.Index(fields=["id", "slug"]),
+            models.Index(fields=["name"]),
+            models.Index(fields=["created"]),
         ]
 
     def __str__(self):
